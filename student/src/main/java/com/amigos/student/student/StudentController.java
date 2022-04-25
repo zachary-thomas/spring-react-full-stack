650bb4c8c2f4ff1c6c3c209820bde43ad081f451
Zac <thomazac@mail.gvsu.edu>
2022-04-25 15:36:50 -0500

    add controller with mock data

---
+++ student/src/main/java/com/amigos/student/student/StudentController.java
@@ -0,0 +1,28 @@
+package com.amigos.student.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return List.of(Student.builder()
+                        .id(1L)
+                        .name("Jamila")
+                        .email("jamila@email.edu")
+                        .gender(Gender.FEMALE)
+                        .build(),
+                Student.builder()
+                        .id(2L)
+                        .name("Alex")
+                        .email("alex@email.edu")
+                        .gender(Gender.MALE)
+                        .build());
+    }
+}
