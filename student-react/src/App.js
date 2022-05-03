5e8d3188d0b5fe47fb5a90c932bce02eb4ab0dac
Zac <thomazac@mail.gvsu.edu>
2022-05-02 20:38:30 -0500

    add proxy

--- student-react/src/App.js
+++ student-react/src/App.js
@@ -1,7 +1,14 @@
 import { Button } from 'antd';
 import './App.css';
 
+import {getAllStudents} from "./Client";
+
 function App() {
+
+  getAllStudents()
+    .then(response => response.json())
+    .then(console.log);
+
   return (
     <div className="App">
       <Button type='primary'>Hello</Button>
