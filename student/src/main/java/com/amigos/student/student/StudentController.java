a894a67e29649152632e32912fff0f02d07c4372
Zac <thomazac@mail.gvsu.edu>
2022-07-17 14:39:01 -0500

    add service

--- student/src/main/java/com/amigos/student/student/StudentController.java
+++ student/src/main/java/com/amigos/student/student/StudentController.java
@@ -1,15 +1,16 @@
 package com.amigos.student.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "api/v1/students")
 public class StudentController {
 
+    private final StudentService studentService;
     @GetMapping
     public List<Student> getAllStudents() {
         return List.of(Student.builder()
---
+++ student/src/main/java/com/amigos/student/student/StudentService.java
@@ -0,0 +1,7 @@
+package com.amigos.student.student;
+
+public class StudentService {
+    
+    public void addStudent(Student student) {
+    }
+}
