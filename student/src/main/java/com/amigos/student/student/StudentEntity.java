91385202386767e96922c55d34287fd7aa517827
Zac <thomazac@mail.gvsu.edu>
2022-08-17 20:23:59 -0500

    add entity support

--- student/src/main/java/com/amigos/student/student/StudentEntity.java
+++ student/src/main/java/com/amigos/student/student/StudentEntity.java
@@ -1,8 +1,19 @@
 package com.amigos.student.student;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
 public class StudentEntity {
+    @Id
     private Long id;
     private String name;
     private String email;
     private Gender gender;
+
 }
